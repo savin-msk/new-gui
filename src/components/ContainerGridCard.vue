22e662b3c5f8dd693928b259888d7f3192ae874a
savin-msk <nick.savin@gmail.com>
2018-02-11 22:50:41 +0300

    CGC fix

--- src/components/ContainerGridCard.vue
+++ src/components/ContainerGridCard.vue
@@ -26,37 +26,27 @@ export default {
         fetchData () {
             this.loading = true
             for (let i in this.containerUrl) {
-                console.log(this)
-                console.log(this.containerUrl[i])
                 let rowitem = {}
                 HTTPS.get(this.containerUrl[i]).then((response) => {
                     let urlArray = this.containerUrl[i].split('/')
                     rowitem.name = urlArray[urlArray.length-1]
-                    console.log(response.data.metadata)
                     rowitem.state = response.data.metadata.status
-                    
-                    console.log(rowitem.IPV4)
-                    console.log(rowitem.IPV6)
                     rowitem.snapshots = ''
-                    console.log(rowitem)
                     this.containerDetails = response.data.metadata
-                    console.log(rowitem)
                 })
                 HTTPS.get(this.stateUrl[i]).then((response) => {
-                    console.log(response.data.metadata.network)
                     if (response.data.metadata.network != null) {
                         let networks = response.data.metadata.network
                         let ifName = Object.keys(networks)[0]
                         rowitem.IPV4 = networks[ifName].addresses[0].address
-                        console.log(networks[ifName].addresses[0].address)
                         rowitem.IPV6 = networks[ifName].addresses[1].address
-                        console.log(networks[ifName].addresses[1].address)
                     }
                     else {
                         rowitem.IPV4 = ''
                         rowitem.IPV6 = ''
                     }
                 })
+                console.log(rowitem)
                 this.items.push(rowitem)
             }
             console.log('this.items')
