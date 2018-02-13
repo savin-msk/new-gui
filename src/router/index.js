adc790036b116c1e98cccf5dabd14a64fd997687
savin-msk <nick.savin@gmail.com>
2018-02-13 09:38:51 +0300

    -

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import axios from 'axios'
 import ServerView from '../components/ServerView.vue'
 import ContainerView from '../components/ContainerView.vue'
-import store from '../store'
-
-import { mapGetters, mapActions } from 'vuex'
 
 Vue.use(Router)
 
 const routes = [
     { 
-      path: '/',
-      name: 'SV',
-      component: ServerView,
+        path: '/',
+        name: 'SV',
+        component: ServerView,
     },
     { 
-      path: '/home',
-      name: 'CV',
-      component: ContainerView
+        path: '/home',
+        name: 'CV',
+        component: ContainerView
     },
-  ]
+]
 
 const router = new Router({ routes })
 
